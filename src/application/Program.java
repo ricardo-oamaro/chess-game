660a3ea9349157c7d2eb1cc3c39e379a2347c612
riamaro <riamaro@br.ibm.com>
2019-06-15 10:27:15 -0300

    board class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Word!");
-
+		
+		Board brd = new Board(3, 5);
+		
+		System.out.println(brd);
 	}
 
 }
